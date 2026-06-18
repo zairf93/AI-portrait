7cda82e2eeef1f002fe5809628768b2e17ce817e
zairf93 <zairf93@gmail.com>
2026-06-18 17:40:22 -0500

    adding print statement to see current url (debugging issue)

--- src/database/sqlalchemy_repo.py
+++ src/database/sqlalchemy_repo.py
@@ -7,6 +7,7 @@ from .base import BaseRepository
 class SQLAlchemyRepository(BaseRepository):
     def __init__(self, db_url: str):
         connect_args = {"check_same_thread": False} if db_url.startswith("sqlite") else {}
+        print(repr(db_url))
         self.engine = create_engine(db_url, connect_args=connect_args, future=True)
         self.metadata = MetaData()
         
